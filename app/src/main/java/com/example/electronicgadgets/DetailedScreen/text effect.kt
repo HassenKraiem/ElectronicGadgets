22bc2d88629cd02c3f20c377bf084b526378ef37
hassen <kraiemhassen26@gmail.com>
2024-10-08 14:23:48 +0100

    adding effects to the detailed screen

--- app/src/main/java/com/example/electronicgadgets/DetailedScreen/text effect.kt
+++ app/src/main/java/com/example/electronicgadgets/DetailedScreen/text effect.kt
@@ -8,36 +8,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import com.example.electronicgadgets.ui.theme.Black01
 import com.example.electronicgadgets.ui.theme.Gris03
 
 @Composable
-fun TextEffect(string: String){
+fun TextEffect(string: String) {
     var isSelected by remember { mutableStateOf(false) }
     var color by remember { mutableStateOf(Gris03) }
     var textDecoration by remember { mutableStateOf(TextDecoration.None) }
     var fontWeight by remember { mutableStateOf(FontWeight(400)) }
-    if (isSelected){
-        color= Black01
-        textDecoration=TextDecoration.Underline
-        fontWeight=FontWeight.Bold
-    }
-    else{
-        color= Gris03
-        textDecoration=TextDecoration.None
-        fontWeight=FontWeight(400)
+    if (isSelected) {
+        color = Black01
+        textDecoration = TextDecoration.Underline
+        fontWeight = FontWeight.Bold
+    } else {
+        color = Gris03
+        textDecoration = TextDecoration.None
+        fontWeight = FontWeight(400)
     }
 
-    Text(
-        text = string,
+    Text(text = string,
         color = color,
         textDecoration = textDecoration,
         fontWeight = fontWeight,
-            modifier = Modifier
-                .clickable { isSelected = !isSelected }
-    )
+        modifier = Modifier.clickable { isSelected = !isSelected })
 
 }
